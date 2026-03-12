d0223079b51a3858513f418930a7a117af2bc7df
Sreytoch <toch8624@gmail.com>
2026-03-12 17:55:14 +0700

    Added delete route for Task 14

--- NodeAPI/index.js
+++ NodeAPI/index.js
@@ -9,5 +9,17 @@ app.get('/view', (req, res) => {
     res.send('<html><body>INSIDE VIEW API..</body></html>');
 });
 
+app.post('/login', (req, res) => {
+    res.send('<html><body>INSIDE LOGIN API..</body></html>');
+});
+
+app.put('/updateprofile', (req, res) => {
+    res.send('<html><body>INSIDE UPDATE PROFILE API..</body></html>');
+});
+
+app.delete('/del', (req, res) => {
+    res.send('<html><body>INSIDE DELETE API..</body></html>');
+});
+
 app.listen(5000, () =>
 console.log('EXPRESS Server Started at Port No: 5000'));
